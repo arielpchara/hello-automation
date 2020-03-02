bb4c1cbe1190e6c9522fd8ba85ab9eaf6d881bbe
Ariel Pchara <ariel@pchara.com>
2020-03-02 00:56:15 -0300

    add builded app

---
+++ app/build/precache-manifest.69dec3299cb582881275dc93afe0adf3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6d0f55e4e9a37ea5a3360fc1eae4e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fa353fa8a265b9f3a76",
+    "url": "/static/js/2.aedf476d.chunk.js"
+  },
+  {
+    "revision": "702bdc716079fa0fd243f36f02ecd130",
+    "url": "/static/js/2.aedf476d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7e42f8900eaba58502b",
+    "url": "/static/js/main.3204ccd3.chunk.js"
+  },
+  {
+    "revision": "a0478af65bd914356dbc",
+    "url": "/static/js/runtime-main.4032331f.js"
+  }
+]);
