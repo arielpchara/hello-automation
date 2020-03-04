135a17798ef1ae23b1d32d7a70cdfedebaaa6f2c
Ariel Pchara <ariel@pchara.com>
2020-03-04 00:52:57 -0300

    app monitor

---
+++ app/build/precache-manifest.33f279475af4efa3eedccb402dac6811.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "540abf001e8fa4728e58379d7baf34a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3853f3dd7faa22729051",
+    "url": "/static/js/2.1f8ce1ee.chunk.js"
+  },
+  {
+    "revision": "eef0880a05ad1a64578791a0f0b159ee",
+    "url": "/static/js/2.1f8ce1ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed72f7ca6a0e8f674240",
+    "url": "/static/js/main.eefa7d61.chunk.js"
+  },
+  {
+    "revision": "a0478af65bd914356dbc",
+    "url": "/static/js/runtime-main.4032331f.js"
+  }
+]);
