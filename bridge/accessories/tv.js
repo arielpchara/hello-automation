173e2eb030823527da914173846df2cf01b70e90
Ariel Pchara <ariel@pchara.com>
2020-06-13 13:14:44 -0300

    tvroom

---
+++ bridge/accessories/tv.js
@@ -0,0 +1,29 @@
+const { createTasmotaAccessory } = require('../../lib/tasmota')
+const { EventEmitter } = require('events')
+
+const tvroom = (mqttClient) => {
+  let state
+  const event = new EventEmitter()
+  mqttClient.subscribe(`stat/tvroom/POWER`)
+  mqttClient.on('message', (topic, payload) => {
+    if( topic === `stat/tvroom/POWER`) {
+      state = payload.toString()
+      event.emit('POWER', state === 'ON')
+    }
+  })
+  return createTasmotaAccessory({
+    name: 'TV Room',
+    Manufacturer: 'Sonoff',
+    model: 'Sonoff Mini',
+    onPower: (value) => {
+      console.log(`OK: ${value}`)
+      mqttClient.publish(`cmnd/tvroom/POWER`, value ? 'ON' : 'OFF')
+    },
+    getPower: () => state === 'ON',
+    event
+  })
+}
+
+module.exports = {
+  tvroom
+}
